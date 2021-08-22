d891dbd3d3ce21e4b245a7098062002a44bd2fcf
hayato sugawara <hayato711@gmail.com>
2021-08-23 00:00:07 +0900

    add firestore config

--- App.tsx
+++ App.tsx
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import firebase from 'firebase/app'
+import "firebase/firestore";
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    const firebaseConfig = {
+      apiKey: "your apiKey",
+      authDomain: "your authDomain",
+      databaseURL: "your databaseURL",
+      projectId: "your projectId",
+      storageBucket: "your storageBucket",
+      messagingSenderId: "your messagingSenderId",
+      appId: "your appId",
+      measurementId: "your measurementId",
+    };
+    firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    useEffect(() => {
+        getFireBaseItems();
+    }, []);
+
+    const getFireBaseItems = async () => {
+        const snapshot = await firebase.firestore().collection("shops").get();
+        const shops = snapshot.docs.map(doc => doc.data());
+        console.log(shops)
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
